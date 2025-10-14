c59382cde53abbf301e6368fbca5b53275862c57
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-14 00:27:25 -0500

    creacion persistence/repository/adapter/UserRepositoryAdapter - RobertoCM

---
+++ src/main/java/com/coworking/application/coworking_booking/persistence/repository/adapter/UserRepositoryAdapter.java
@@ -0,0 +1,16 @@
+package com.coworking.application.coworking_booking.persistence.repository.adapter;
+
+import com.coworking.application.coworking_booking.business.repository.UserRepositoryPort;
+import com.coworking.application.coworking_booking.persistence.entity.UserEntity;
+import com.coworking.application.coworking_booking.persistence.repository.spring.UserJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+
+@Component
+@RequiredArgsConstructor
+public class UserRepositoryAdapter implements UserRepositoryPort {
+	
+	
+}
