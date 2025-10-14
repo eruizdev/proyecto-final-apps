aa24932bcfa7e67167a2205d397addf98f1fb938
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-13 23:30:23 -0500

    creacion infraestructure/config/DataSeeder - RobertoCM

---
+++ src/main/java/com/coworking/application/coworking_booking/infraestructure/config/DataSeeder.java
@@ -0,0 +1,16 @@
+package com.coworking.application.coworking_booking.infraestructure.config;
+
+import com.coworking.application.coworking_booking.persistence.entity.*;
+import com.coworking.application.coworking_booking.persistence.repository.spring.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Configuration
+public class DataSeeder {
+	
+	
+}
