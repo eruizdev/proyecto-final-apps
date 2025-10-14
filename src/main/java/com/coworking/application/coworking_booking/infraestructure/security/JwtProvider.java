3220b6559924fea04a98eb722421dc99a9a7c81e
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-13 23:53:21 -0500

    creacion infraestructure/security/SecurityConfig - RobertoCM

--- src/main/java/com/coworking/application/coworking_booking/infraestructure/security/JwtProvider.java
+++ src/main/java/com/coworking/application/coworking_booking/infraestructure/security/JwtProvider.java
@@ -69,5 +69,5 @@ public class JwtProvider {
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token);
-    }
+    }	
 }
---
+++ src/main/java/com/coworking/application/coworking_booking/infraestructure/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.coworking.application.coworking_booking.infraestructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Configuración de seguridad general para el backend.
+ * Integra JWT, roles y filtros personalizados.
+ */
+@Configuration
+@EnableMethodSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	
+}
