13ad3a0a24b2e4222ad0066d8aa095ef06774612
Camilo <marincamilo505@gmail.com>
2025-10-14 00:37:08 -0500

    Autenticacion y autorizacion del LoginRequestDTO

---
+++ src/main/java/com/coworking/application/coworking_booking/presentation/dto/auth/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+// autenticacion y autorizacion
+
+package main.java.com.coworking.application.coworking_booking.presentation.dto.auth;
+
+public record LoginRequestDTO(String email, String password) {}
