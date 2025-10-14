a6b80398c8a045542a1e23f1356d3f3af33d8ffe
Grimi2205 <grimaldos.samuel.8495@eam.edu.co>
2025-10-14 02:23:24 -0500

    metodo para el registro de usuarios

--- src/main/java/com/coworking/application/coworking_booking/business/service/impl/AuthServiceImpl.java
+++ src/main/java/com/coworking/application/coworking_booking/business/service/impl/AuthServiceImpl.java
@@ -16,3 +16,27 @@ public class AuthServiceImpl implements AuthService {
 
     private final UserRepositoryPort users;
     private final JwtProvider jwt; // inyectamos JwtProvider para generar el token
+
+     @Override
+    public UserEntity register(String email, String pass, String first, String last) {
+        var now = LocalDateTime.now();
+        var u = UserEntity.builder()
+                .email(email)
+                .passwordHash("{noop}" + pass) // MVP: sin encriptar (usado con {noop} para Spring Security)
+                .firstName(first)
+                .lastName(last)
+                .userRole(UserEntity.Role.USER)
+                .active(true)
+                .emailVerified(true)
+                .createdAt(now)
+                .updatedAt(now)
+                .build();
+        return users.save(u);
+    }
+
+    @Override
+    public UserEntity get(Long id) {
+        return users.require(id);
+    }
+
+    
