d9e281a250ea1a142e8675c7da53a7b1d63cde1a
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-13 23:39:13 -0500

    creacion infraestructure/config/SwaggerConfig - RobertoCM

---
+++ src/main/java/com/coworking/application/coworking_booking/infraestructure/config/SwaggerConfig.java
@@ -0,0 +1,6 @@
+package com.coworking.application.coworking_booking.infraestructure.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig { /* springdoc auto-config */ }
