18530e48f24c3b7c8c7aee7be7ba863cd5d5ee86
Grimi2205 <grimaldos.samuel.8495@eam.edu.co>
2025-10-13 23:37:52 -0500

    excepcion, excepcion no encontrada

---
+++ src/main/java/com/coworking/application/coworking_booking/business/exception/NotFoundException.java
@@ -0,0 +1,6 @@
+package main.java.com.coworking.application.coworking_booking.business.exception;
+
+public class NotFoundException extends BusinessException {
+  public NotFoundException(String m){ super(m); }
+}
+
