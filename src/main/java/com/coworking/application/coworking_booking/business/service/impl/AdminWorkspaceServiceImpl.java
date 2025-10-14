a607a14d6ed98eaf2e5324c75bfdc03e0a06413e
Grimi2205 <grimaldos.samuel.8495@eam.edu.co>
2025-10-14 01:55:24 -0500

    final metodos espacioscur.setName(s.getName());

--- src/main/java/com/coworking/application/coworking_booking/business/service/impl/AdminWorkspaceServiceImpl.java
+++ src/main/java/com/coworking/application/coworking_booking/business/service/impl/AdminWorkspaceServiceImpl.java
@@ -84,10 +84,3 @@ public class AdminWorkspaceServiceImpl implements AdminWorkspaceService {
     return spaces.save(cur);
   }
 
-  @Override
-  public void deleteSpace(Long id) { spaces.deleteById(id); }
-
-  @Override
-  public List<SpaceEntity> listSpaces() { return spaces.findAll(); }
-}
-
