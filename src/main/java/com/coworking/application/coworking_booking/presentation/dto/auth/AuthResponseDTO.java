f590c93add34d1c23e1eaa74f3e64f99f7545f81
Camilo <marincamilo505@gmail.com>
2025-10-14 00:36:27 -0500

    Autenticacion y autorizacion del AuthResponseDTO

---
+++ src/main/java/com/coworking/application/coworking_booking/presentation/dto/auth/AuthResponseDTO.java
@@ -0,0 +1,4 @@
+// autenticacion y autorizacion
+
+package main.java.com.coworking.application.coworking_booking.presentation.dto.auth;
+public record AuthResponseDTO(Long id, String email, String role) {}
