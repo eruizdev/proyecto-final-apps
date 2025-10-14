70107204a74d13e9076d1245913633b475fe1923
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-13 23:49:23 -0500

    creacion infraestructure/security/JwtProvider - RobertoCM

---
+++ src/main/java/com/coworking/application/coworking_booking/infraestructure/security/JwtProvider.java
@@ -0,0 +1,16 @@
+package com.coworking.application.coworking_booking.infraestructure.security;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+public class JwtProvider {
+	
+}
