860f545c583ae9035165aaed8e94d39a8d2b5c49
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-13 23:43:49 -0500

    complemento de clase infraestructure/security/CustomUserDetailsService - RobertoCM

--- src/main/java/com/coworking/application/coworking_booking/infraestructure/security/CustomUserDetailsService.java
+++ src/main/java/com/coworking/application/coworking_booking/infraestructure/security/CustomUserDetailsService.java
@@ -10,5 +10,17 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-	
+	private final UserJpaRepository users;
+
+	  @Override
+	  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+	    var u = users.findAll().stream()
+	        .filter(x -> x.getEmail().equalsIgnoreCase(email))
+	        .findFirst()
+	        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+	    return new org.springframework.security.core.userdetails.User(
+	        u.getEmail(), u.getPasswordHash(),
+	        List.of(() -> "ROLE_" + u.getUserRole().name())
+	    );
+	  }
 }
