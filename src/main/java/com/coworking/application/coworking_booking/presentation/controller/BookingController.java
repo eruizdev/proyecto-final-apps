f527d7e872dcf9e6284dd1fe4e4bf57019e49b22
Camilo <marincamilo505@gmail.com>
2025-10-13 23:40:59 -0500

    Agregacion de importes

---
+++ src/main/java/com/coworking/application/coworking_booking/presentation/controller/BookingController.java
@@ -0,0 +1,13 @@
+package main.java.com.coworking.application.coworking_booking.presentation.controller;
+
+import com.coworking.application.coworking_booking.business.service.BookingService;
+import com.coworking.application.coworking_booking.presentation.dto.booking.*;
+import org.springframework.http.*;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/bookings")
+public class BookingController {
+    
+}
