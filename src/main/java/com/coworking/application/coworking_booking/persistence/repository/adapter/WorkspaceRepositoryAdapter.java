e15c6a0fa703a5ae3212329f3fc7853a3d7033e6
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-14 00:30:18 -0500

    creacion persistence/repository/adapter/WorkspaceRepositoryAdapter - RobertoCM

---
+++ src/main/java/com/coworking/application/coworking_booking/persistence/repository/adapter/WorkspaceRepositoryAdapter.java
@@ -0,0 +1,16 @@
+package com.coworking.application.coworking_booking.persistence.repository.adapter;
+
+import com.coworking.application.coworking_booking.business.repository.*;
+import com.coworking.application.coworking_booking.persistence.entity.*;
+import com.coworking.application.coworking_booking.persistence.repository.spring.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Component @RequiredArgsConstructor
+public class WorkspaceRepositoryAdapter implements WorkspaceRepositoryPort {
+	
+	
+}
