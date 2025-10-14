13f9d003e95e27c8c8abd23d37b06359f2a8e900
Camilo <marincamilo505@gmail.com>
2025-10-13 22:22:41 -0500

    server port add

---
+++ src/main/java/com/coworking/application/coworking_booking/business/exception/BusinessException.java
@@ -0,0 +1,6 @@
+package com.coworking.application.coworking_booking.business.exception;
+
+public class BusinessException extends RuntimeException {
+  public BusinessException(String m){ super(m); }
+}
+
