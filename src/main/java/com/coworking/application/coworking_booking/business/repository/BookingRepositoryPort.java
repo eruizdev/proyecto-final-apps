dd336d9f7d99fd64c798fea3d886685c5b72a16f
Grimi2205 <grimaldos.samuel.8495@eam.edu.co>
2025-10-14 00:46:17 -0500

    puerto repositorio para booking

---
+++ src/main/java/com/coworking/application/coworking_booking/business/repository/BookingRepositoryPort.java
@@ -0,0 +1,10 @@
+package com.coworking.application.coworking_booking.business.repository;
+
+import com.coworking.application.coworking_booking.persistence.entity.BookingEntity;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
+
+public interface BookingRepositoryPort {
+    BookingEntity save(BookingEntity b);
+    Optional<BookingEntity> findById(Long id);
