17f9855d61fc8861357a38889075ab982f63fc6b
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-14 00:22:50 -0500

    creacion persistence/repository/adapter/NotificationRepositoryAdapter - RobertoCM

---
+++ src/main/java/com/coworking/application/coworking_booking/persistence/repository/adapter/NotificationRepositoryAdapter.java
@@ -0,0 +1,15 @@
+package com.coworking.application.coworking_booking.persistence.repository.adapter;
+
+import com.coworking.application.coworking_booking.business.repository.NotificationRepositoryPort;
+import com.coworking.application.coworking_booking.persistence.entity.NotificationEntity;
+import com.coworking.application.coworking_booking.persistence.repository.spring.NotificationJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationRepositoryAdapter implements NotificationRepositoryPort {
+	
+	
+}
