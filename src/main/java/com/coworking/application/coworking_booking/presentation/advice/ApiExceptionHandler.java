c91e3a6fedefc98776e49f6e3656d902a0c45486
Camilo <marincamilo505@gmail.com>
2025-10-13 23:25:05 -0500

    Agregamos importes necesarios para ApiExceptionHadler

--- src/main/java/com/coworking/application/coworking_booking/presentation/advice/ApiExceptionHandler.java
+++ src/main/java/com/coworking/application/coworking_booking/presentation/advice/ApiExceptionHandler.java
@@ -6,3 +6,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.Instant;
 import java.util.Map;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    // Metodo auxiliar para crear el cuerpo del error
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<?> notFound(NotFoundException ex){
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err("NOT_FOUND", ex.getMessage()));
+  }
+}
