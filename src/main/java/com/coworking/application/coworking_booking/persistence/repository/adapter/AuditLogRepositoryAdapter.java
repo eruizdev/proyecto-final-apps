18358ad6619771364451e326d0981bf783b919d0
RC_MEDINA <medina.roberto.3800@eam.edu.co>
2025-10-14 00:19:02 -0500

    creacion persistence/repository/adapter/AuditLogRepositoryAdapter - RobertoCM

---
+++ src/main/java/com/coworking/application/coworking_booking/persistence/repository/adapter/AuditLogRepositoryAdapter.java
@@ -0,0 +1,15 @@
+package com.coworking.application.coworking_booking.persistence.repository.adapter;
+
+import com.coworking.application.coworking_booking.business.repository.AuditLogRepositoryPort;
+import com.coworking.application.coworking_booking.persistence.entity.AuditLogEntity;
+import com.coworking.application.coworking_booking.persistence.repository.spring.AuditLogJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AuditLogRepositoryAdapter implements AuditLogRepositoryPort {
+	
+	
+	
+}
