c440857d3dbb77792b6e81e195433ca3688845e1
Grimi2205 <grimaldos.samuel.8495@eam.edu.co>
2025-10-14 00:19:07 -0500

    validacion de politicas terminada

--- src/main/java/com/coworking/application/coworking_booking/business/policy/BookingPolicies.java
+++ src/main/java/com/coworking/application/coworking_booking/business/policy/BookingPolicies.java
@@ -1,4 +1,4 @@
-package src.main.java.com.coworking.application.coworking_booking.business.policy;
+package com.coworking.application.coworking_booking.business.policy;
 
 import com.coworking.application.coworking_booking.business.exception.ValidationException;
 import com.coworking.application.coworking_booking.persistence.entity.SpaceEntity;
@@ -11,10 +11,20 @@ public class BookingPolicies {
                                int attendees, boolean hasOverlap, int futureCount) {
     if (!start.isBefore(end)) throw new ValidationException("Rango inválido (inicio>=fin)");
     long hours = Duration.between(start, end).toHours();
-   if (hours < 1 || hours > 8) throw new ValidationException("Reserva mínima 1h y máxima 8h");
+    if (hours < 1 || hours > 8) throw new ValidationException("Reserva mínima 1h y máxima 8h");
     if (Duration.between(LocalDateTime.now(), start).toDays() > 30)
       throw new ValidationException("No más de 30 días de anticipación");
     if (Duration.between(LocalDateTime.now(), start).toHours() < 2)
       throw new ValidationException("Crear/modificar mínimo con 2 horas");
+    if (attendees > space.getCapacity()) throw new ValidationException("Excede capacidad");
+    if (hasOverlap) throw new ValidationException("Horario no disponible");
+    if (futureCount >= 5) throw new ValidationException("Máximo 5 reservas futuras");
+    if (space.getSpaceStatus() != SpaceEntity.SpaceStatus.AVAILABLE)
+      throw new ValidationException("Espacio no disponible");
+  }
 
+  public boolean penalize50(LocalDateTime start, LocalDateTime now){
+    return Duration.between(now, start).toHours() < 24;
+  }
+}
 
