9577f780f397fd2d142bb566192be286e68d2456
Camilo <marincamilo505@gmail.com>
2025-10-16 00:02:32 -0500

    Creamos el @Test para verificar que la validacion falle cuando la capacidad es insuficiente en una reserva

--- src/test/java/com/coworking/application/coworking_booking/BookingPoliciesTest.java
+++ src/test/java/com/coworking/application/coworking_booking/BookingPoliciesTest.java
@@ -12,13 +12,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BookingPoliciesTest {
 
-// Test para validar rango, capacidad y límites
+// Test para validar rango, capacidad y límites de una reserva
  @Test
   void validaRangoYCapacidadYLimites() {
     var space = SpaceEntity.builder().capacity(5).spaceStatus(SpaceEntity.SpaceStatus.AVAILABLE).build();
     var start = LocalDateTime.now().plusHours(3);
     var end = start.plusHours(2);
     assertDoesNotThrow(() -> new BookingPolicies().validateCreation(space, start, end, 5, false, 0));
+  }
+
+  // Test para verificar que la validación falle cuando la capacidad es insuficiente
+  @Test
+  void fallaPorSolapamiento() {
+    var space = SpaceEntity.builder().capacity(5).spaceStatus(SpaceEntity.SpaceStatus.AVAILABLE).build();
+    var start = LocalDateTime.now().plusHours(3);
+    var end = start.plusHours(2);
+    assertThrows(ValidationException.class,
+        () -> new BookingPolicies().validateCreation(space, start, end, 4, true, 0));
   }
 
 }
