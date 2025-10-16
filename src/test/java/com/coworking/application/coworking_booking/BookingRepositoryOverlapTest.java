4e32f6459f6337067b70b5bbc9a395b0503ea4f2
Camilo <marincamilo505@gmail.com>
2025-10-16 00:08:20 -0500

    Configuramos importes y anotaciones necesarias para el BookingRepositoryTest

---
+++ src/test/java/com/coworking/application/coworking_booking/BookingRepositoryOverlapTest.java
@@ -0,0 +1,20 @@
+package com.coworking.application.coworking_booking;
+
+// importes necesarios
+import com.coworking.application.coworking_booking.persistence.entity.*;
+import com.coworking.application.coworking_booking.persistence.repository.spring.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Anotaciones para configurar el entorno de pruebas
+@DataJpaTest
+class BookingRepositoryOverlapTest {
+
+}
