dace7da0d0a6f3b3d8fdd4a0520ed643885ede77
Camilo <marincamilo505@gmail.com>
2025-10-15 23:41:39 -0500

    Agregamos anotaciones y dependencias iniciales para AuditNotificationTest

---
+++ src/test/java/com/coworking/application/coworking_booking/AuditNotificationIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.coworking.application.coworking_booking;
+
+import com.coworking.application.coworking_booking.business.service.BookingService;
+import com.coworking.application.coworking_booking.persistence.entity.*;
+import com.coworking.application.coworking_booking.persistence.repository.spring.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class NotificationAuditIntegrationTest {
+
+}
