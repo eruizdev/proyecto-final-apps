a6e1fe2ff657843627fb6ac8dd2a1a28c472609a
ERUIZDEV <jeruizdev@gmail.com>
2025-10-17 19:22:24 -0500

    se añadio lista en booking para users

--- src/main/java/com/coworking/application/coworking_booking/business/service/BookingService.java
+++ src/main/java/com/coworking/application/coworking_booking/business/service/BookingService.java
@@ -1,12 +1,13 @@
 package com.coworking.application.coworking_booking.business.service;
 
-import com.coworking.application.coworking_booking.persistence.entity.*;
+import com.coworking.application.coworking_booking.persistence.entity.BookingEntity;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public interface BookingService {
   BookingEntity create(Long userId, Long spaceId, LocalDateTime start, LocalDateTime end, int attendees);
   BookingEntity get(Long id);
-  void cancel(Long bookingId);
+  void cancel(Long id);
+  List<BookingEntity> listByUser(Long userId, int limit);
 }
-
